962d2564225c0b3a35ca4ba2e9e5c405f8d659af
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2024-06-19 23:31:48 -0500

    remove not useful recording all bool

--- record.py
+++ record.py
@@ -6,7 +6,6 @@ import sys
 
 # Global variable
 storage: list = []
-record_all: bool = False
 
 def on_press(key):
     try:
@@ -23,25 +22,15 @@ def on_release(key):
     except AttributeError:
         json_object = {'action':'released_key', 'key':str(key), '_time': time.time()}
     storage.append(json_object)
-        
 
 def on_move(x, y):
-    if (record_all) == True:
-        if len(storage) >= 1:
-            if storage[-1]['action'] != "moved":
-                json_object = {'action':'moved', 'x':x, 'y':y, '_time':time.time()}
-                storage.append(json_object)
-            elif storage[-1]['action'] == "moved" and time.time() - storage[-1]['_time'] > 0.02:
-                json_object = {'action':'moved', 'x':x, 'y':y, '_time':time.time()}
-                storage.append(json_object)
-        else:
+    if len(storage) >= 1:
+        if storage[-1]['action'] != "moved":
             json_object = {'action':'moved', 'x':x, 'y':y, '_time':time.time()}
             storage.append(json_object)
-    else:
-        if len(storage) >= 1:
-            if (storage[-1]['action'] == "pressed" and storage[-1]['button'] == 'Button.left') or (storage[-1]['action'] == "moved" and time.time() - storage[-1]['_time'] > 0.02):
-                json_object = {'action':'moved', 'x':x, 'y':y, '_time':time.time()}
-                storage.append(json_object)
+        elif storage[-1]['action'] == "moved" and time.time() - storage[-1]['_time'] > 0.02:
+            json_object = {'action':'moved', 'x':x, 'y':y, '_time':time.time()}
+            storage.append(json_object)
 
 def on_click(x, y, button, pressed):
     json_object = {'action':'pressed' if pressed else 'released', 'button':str(button), 'x':x, 'y':y, '_time':time.time()}
@@ -52,22 +41,11 @@ def on_scroll(x, y, dx, dy):
     storage.append(json_object)
 
 def main():
-    n = len(sys.argv)
 
-    if n < 2:
-        exit("Takes a compulsory argument - name of recording, and optional argument - record-all")
-
-    if n > 3:
-        exit("Only takes two arguments - name of recording and (optional) record-all")
-
-    if n == 2:
+    if len(sys.argv) == 2:
         name_of_recording = str(sys.argv[1])
-        record_all = False
-    if n == 3:
-        if str(sys.argv[2]) != "record-all":
-            exit("The second argument given must be 'record-all', otherwise only pass the name of recording as a parameter")
-        name_of_recording = str(sys.argv[1])
-        record_all = True
+    else:
+        exit("Please supply recording name")
 
     print("Press 'esc' to end recording.")
 
